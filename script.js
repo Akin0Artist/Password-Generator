696403dc6355040da37c7b3e9058766b9d7eef45
Akin0Artist <sanakino778@gmail.com>
2024-06-22 14:18:33 +0100

    Add files via upload

---
+++ script.js
@@ -0,0 +1,14 @@
+function generateRandomPassword(length) {
+    const charset = "abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ0123456789!@#$%^&*()";
+    let password = "";
+    for (let i = 0; i < length; i++) {
+        const randomIndex = Math.floor(Math.random() * charset.length);
+        password += charset[randomIndex];
+    }
+    return password;
+}
+
+document.getElementById('Button').addEventListener('click', function() {
+    const password = generateRandomPassword(12); // Generate a 12-character password
+    document.getElementById('pass').innerHTML = password;
+});
